a0d9f24b6b3c9e1df4cb7ea53673abcbc4041c9d
John Claude <johnc@korbicom.com>
2022-08-14 17:58:41 +0200

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,41 +1,26 @@
-import React from 'react';
-import styled from '@emotion/styled';
-import { colors, ApolloIcon } from '../styles';
+import React from "react";
+import styled from "@emotion/styled";
+import { colors } from "../styles";
 
 /**
  * Footer is useless component to make our app look a little closer to a real website!
  */
 const Footer = ({ children }) => {
-  return (
-    <FooterContainer>
-      2021 ©{' '}
-      <LogoContainer>
-        <ApolloIcon width="100px" height="40px" />
-      </LogoContainer>
-    </FooterContainer>
-  );
+  return <FooterContainer>2022 © johnclaude.com</FooterContainer>;
 };
 
 export default Footer;
 
 /** Footer styled components */
 const FooterContainer = styled.div({
-  display: 'flex',
-  flexDirection: 'row',
-  justifyContent: 'center',
-  alignItems: 'center',
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "center",
+  alignItems: "center",
   color: colors.pink.base,
   marginTop: 30,
   height: 200,
   padding: 20,
-  backgroundColor: 'white',
+  backgroundColor: "white",
   borderTop: `solid 1px ${colors.pink.light}`,
-});
-
-const LogoContainer = styled.div({
-  height: 40,
-  marginLeft: 5,
-  svg: {
-    height: 40,
-  },
 });
