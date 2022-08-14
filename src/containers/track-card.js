70b11bb2fa7020a3b43f5f1a536628a89eb08d79
John Claude <johnc@korbicom.com>
2022-08-14 18:33:50 +0200

    missed field

--- src/containers/track-card.js
+++ src/containers/track-card.js
@@ -24,9 +24,10 @@ const INCREMENT_TRACK_VIEWS = gql`
  * for each track populating the tracks grid homepage.
  */
 const TrackCard = ({ track }) => {
-  const { title, thumbnail, author, length, modulesCount, id } = track;
+  const { title, thumbnail, author, durationInSeconds, modulesCount, id } =
+    track;
   const [incrementTrackViews] = useMutation(INCREMENT_TRACK_VIEWS, {
-    variables: {incrementTrackViewsId: id},
+    variables: { incrementTrackViewsId: id },
   });
 
   return (
@@ -42,7 +43,8 @@ const TrackCard = ({ track }) => {
             <AuthorAndTrack>
               <AuthorName>{author.name}</AuthorName>
               <TrackLength>
-                {modulesCount} modules - {humanReadableTimeFromSeconds(length)}
+                {modulesCount} modules -{" "}
+                {humanReadableTimeFromSeconds(durationInSeconds)}
               </TrackLength>
             </AuthorAndTrack>
           </CardFooter>
